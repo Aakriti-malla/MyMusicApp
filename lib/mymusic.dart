530d71e7da8e342e530d81d77a140f0987d1d202
Aakriti <aakritimalla91@gmail.com>
2021-11-16 12:28:38 +0530

    changes

--- lib/mymusic.dart
+++ lib/mymusic.dart
@@ -1,33 +1,33 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:audioplayers/src/audio_cache.dart';
+import 'song.dart';
 
-
-import 'main.dart';
-void main() {
-  runApp(const MyApp());
-}
 class MusicApp extends StatefulWidget {
-  const MusicApp({Key? key}) : super(key: key);
+   const MusicApp({ required this.msong}) ;
+  final SongInfo msong;
 
   @override
   _MusicAppState createState() => _MusicAppState();
 }
 
-class _MusicAppState extends State<MusicApp> {
+class _MusicAppState extends State<MusicApp>{
+  var bgColor=  const Color(0xFFCCD74D);
+  var iconHoverColor = const Color(0xFF0F7220);
   bool playing = false;
   IconData playBtn = Icons.play_circle_outline;
   AudioPlayer _player= AudioPlayer();
   AudioCache cache= AudioCache();
-  Duration position = new Duration();
-  Duration musicLength = new Duration();
+  Duration position = Duration();
+  Duration musicLength = Duration();
 
   Widget slider() {
     return Container(
       width: 300,
       child: Slider.adaptive(
-        activeColor: Colors.blueAccent,
-        inactiveColor: Colors.grey,
+        activeColor: Colors.greenAccent,
+        inactiveColor: Colors.blueGrey,
         value: position.inSeconds.toDouble(),
         max:musicLength.inSeconds.toDouble(),
         onChanged: (value)
@@ -57,158 +57,208 @@ class _MusicAppState extends State<MusicApp> {
       });
     });
 
+
   }
 
-
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor:  Color(0xFF18821F),
       body: Container(
-        decoration:BoxDecoration(gradient: LinearGradient(begin:Alignment.topLeft,
-            end:Alignment.bottomRight,
-            colors:[Colors.pink,
-              Colors.pinkAccent,]),
-        ),
-        child:Padding(
-          padding:EdgeInsets.only(
-            top:40.0,
-          ),
-          child: Container(
-            child:Column(
-              mainAxisAlignment:MainAxisAlignment.start,
-              crossAxisAlignment:CrossAxisAlignment.start,
-              children:[
-                Padding(
-                  padding:const EdgeInsets.only(left:12.0),
-                  child:Text(
-                    "Musically",
-                    style:TextStyle(
-                      color: Colors.white,
-                      fontSize: 38.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height:24.0,
-                ),
-                Center(
-                  child:Container(
-                    width: 280.0,
-                    height: 280.0,
-                    decoration: BoxDecoration(
-                      borderRadius:BorderRadius.circular(30.0),
-                      image:DecorationImage(
-                        image: AssetImage("assets/1st.gif"),
-                      ),
-                    ),
-
-                  ),
-                ),
-                SizedBox(
-                  height:20.0,
-                ),
-                Center(
-                  child: Text(
-                    "its you",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 22.0,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height:30.0,
-                ),
-                Expanded(child: Container(
-                  decoration:BoxDecoration(
-                    color:Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(30.0),
-                      topRight: Radius.circular(30.0),
-
-                    ),
-                  ),
-                  child: Column(
-                    mainAxisAlignment:MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children:[
-                      Container(
-                        width:500.0,
-                        child:Row(
-                          mainAxisAlignment:MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children:[
-                            Text("${position.inMinutes}:${position.inSeconds.remainder(60)}",
-                              style: TextStyle(fontSize:18.0,),
-                            ),
-                            slider(),
-                            Text("${musicLength.inMinutes}:${musicLength.inSeconds.remainder(60)}",
-                              style: TextStyle(fontSize:18.0,),
-
-                            ),
-                          ],
+        child: Center(
+          child: Column(
+            children: [
+              Container(
+                height: 500.0,
+                child: Stack(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(widget.msong.image),
+                          fit: BoxFit.cover,
                         ),
-
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [bgColor.withOpacity(0.4), bgColor],
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                        ),
+                      ),
+                    ),
+                    Padding(padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                      child: Column(
                         children: [
-                          IconButton(
-                            iconSize: 45.0,
-                            color:Colors.blue[500],
-                            onPressed:(){
+                          SizedBox(
+                            height: 40.0,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.white.withOpacity(0.1),
+                                    borderRadius: BorderRadius.circular(50.0)),
+                                child: Icon(
+                                  Icons.arrow_drop_down,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Column(
+                                children: [
+                                  Text(
+                                    'PLAYLIST',
+                                    style: TextStyle(
+                                        color: Colors.white.withOpacity(0.6)),
+                                  ),
+                                  Text('Your favourite music',
+                                      style: TextStyle(color: Colors.white)),
+                                ],
+                              ),
+                              Icon(
+                                Icons.playlist_add,
+                                color: Colors.white,
+                              )
+                            ],
+                          ),
+                          SizedBox(
+                            height:30.0,
+                          ),
+                          Text(widget.msong.title,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 32.0)),
+                          SizedBox(
+                            height: 6.0,
+                          ),
+                          Text(
+                            widget.msong.singer,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18.0),
+                          ),
+                          SizedBox(height: 10.0),
 
-                            },
-                            icon: Icon(
-                              Icons.skip_previous,
+                          Center(
+                            child: Container(
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: AssetImage(
+                                          widget.msong.image),
+                                      fit: BoxFit.cover),
+                                ),
+                                child: BackdropFilter(
+                                  filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
+                                )
                             ),
                           ),
-                          IconButton(
-                            iconSize: 60.0,
-                            color:Colors.blue[800],
-                            onPressed:(){
-                              if(!playing){
-                                cache.play("song.mp3");
-                                setState((){
-                                  playBtn=Icons.pause_circle_outline;
-                                  playing=true;
-                                });
-                              }
-                              else{
-                                _player.pause();
-                                setState((){
-                                  playBtn=Icons.play_circle_outline;
-                                  playing=false;
-                                });
-                              }
-
-                            },
-
-                            icon: Icon(
-                                playBtn
-                            ),
-                          ),
-                          IconButton(
-                            iconSize: 45.0,
-                            color:Colors.blue[500],
-                            onPressed:(){},
-                            icon: Icon(
-                              Icons.skip_next,
-                            ),
-                          ),
 
 
-                        ],),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 5.0,
+              ),
+
+
+              Column(
+                mainAxisAlignment:MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children:[
+                  Container(
+                    width:500.0,
+                    child:Row(
+                      mainAxisAlignment:MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children:[
+                        Text("${position.inMinutes}:${position.inSeconds.remainder(60)}",
+                          style: TextStyle(fontSize:18.0,color: Colors.white,),
+                        ),
+                        slider(),
+                        Text("${musicLength.inMinutes}:${musicLength.inSeconds.remainder(60)}",
+                          style: TextStyle(fontSize:18.0,color: Colors.white,),
+
+                        ),
+                      ],
+                    ),
+
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.fast_rewind,
+                        color: Colors.white54,
+                        size: 30.0,
+                      ),
+                      IconButton(
+                        iconSize: 40.0,
+                        color:Colors.blue[500],
+                        onPressed:(){
+
+                        },
+                        icon: Icon(
+                          Icons.skip_previous,
+                        ),
+                      ),
+                      IconButton(
+                        iconSize: 55.0,
+                        color:Colors.blue[800],
+                        onPressed:(){
+                          if(!playing){
+                            cache.play(widget.msong.path);
+                            setState((){
+                              playBtn=Icons.pause_circle_outline;
+                              playing=true;
+                            });
+                          }
+                          else{
+                            _player.pause();
+                            setState((){
+                              playBtn=Icons.play_circle_outline;
+                              playing=false;
+                            });
+                          }
+
+                        },
+
+                        icon: Icon(
+                            playBtn
+                        ),
+                      ),
+                      IconButton(
+                        iconSize: 40.0,
+                        color:Colors.blue[500],
+                        onPressed:(){},
+                        icon: Icon(
+                          Icons.skip_next,
+                        ),
+                      ),
+                      Icon(
+                        Icons.fast_forward,
+                        color: Colors.white54,
+                        size: 30.0,
+                      ),
+
+
+
                     ],),
+                ],),
 
-                ),),],
-            ),
+              SizedBox(height: 30.0),
+            ],
           ),
         ),
-      ), );
-
+      ),
+    );
   }
 }
--- lib/song.dart
+++ lib/song.dart
@@ -1,19 +1,173 @@
-class SongInfo {
-  String songName;
-  String songPath;
-  String imagePath;
-// YOU CAN ADD MORE VARIABLES TO STORE INFO LIKE SINGER, BAND ETC.
-  SongInfo(this.songName, this.songPath, this.imagePath);
-}
-List<SongInfo> songs = [
-  SongInfo('Good Days', 'assets/Eastside.mp3', 'assets/good_days.jfif'),
-  SongInfo('Eastside', 'assets/Eastside.mp3', 'assets/eastside.jfif'),
-  SongInfo('Formula', 'assets/Formula.mp3', 'assets/euphoria.jfif'),
-  SongInfo('Forever', 'assets/Forever.mp3', 'assets/euphoria.jfif'),
-  SongInfo('Go with u Remix', 'assets/go_with_u.mp3', 'assets/gowithu.jfif')
+import 'package:flutter/material.dart';
+import 'main.dart';
+import 'mymusic.dart';
 
-];
-Text(
+void main() {
+  runApp(const MyApp());
+}
+class MyMusic extends StatefulWidget {
+  const MyMusic({Key? key}) : super(key: key);
+
+  @override
+  _MyMusicState createState() => _MyMusicState();
+}
+class SongInfo {
+  String title;
+  String singer;
+  String image;
+  String path;
+  SongInfo( {required this.title, required this.singer, required this.image, required this.path});
+
+
+}
+
+class _MyMusicState extends State<MyMusic> {
+  late List song;
+  void initState() {
+    song = getList();
+    super.initState();
+}
+
+  List getList(){
+    return[
+      SongInfo(title: 'Good Days',singer: 'SZA' ,image:"assets/gooddays.jpg" ,
+          path: "assets/Good_Days.mp3"),
+      SongInfo(title: 'Forever',singer: 'Labrinth',image:"assets/euphoria.jpeg",
+          path: "assets/Forever.mp3"),
+      SongInfo(title: 'Formula',singer: 'Labrinth',image: "assets/euphoria.jpeg",
+          path: "assets/Formula.mp3"),
+      SongInfo(title: 'Go with u Remix',singer: 'Baynk',image: "assets/gowithu.jpg",
+          path: "assets/go_with_u.mp3"),
+      SongInfo(title: 'Eastside',singer:'Benny Blanco ft. Halsey,khalid',image: "assets/eastside.jpg",
+          path:"assets/Eastside.mp3"),
+      SongInfo(title: 'Mystery Of Love',singer:'Sufjan Stevens',image: "assets/mystery.jpg",
+          path:"assets/Mystery_of_Love.mp3"),
+      SongInfo(title: 'free love',singer: 'Honne',image: "freelove.jpg",
+          path:"assets/free_love.mp3"),
+      SongInfo(title: 'Wander',singer: 'Keegan Dewitt' ,image:"wander.jpg",
+          path:"assets/wander.mp3"),
+
+
+    ];
+  }
+  Widget customListView({
+    required String title,
+    required String singer,
+    required String image,
+    required String path,
+    onTap,
+  }) {
+    return InkWell(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Row(
+          children: [
+            Stack(children: [
+              Container(
+                height: 80.0,
+                width: 80.0,
+                child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8.0),
+                    child:Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(image),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    )
+                ),
+              ),
+              Container(
+                height: 80.0,
+                width: 80.0,
+                child: Icon(
+                  Icons.play_circle_filled,
+                  color: Colors.white.withOpacity(0.7),
+                  size: 42.0,
+                ),
+              )
+            ]),
+            const SizedBox(width: 10.0),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                      color: Colors.white60,
+                      fontWeight: FontWeight.normal,
+                      fontSize: 20.0
+                  ),
+                ),
+                const SizedBox(height: 8.0),
+                Text(
+                  singer,
+                  style: TextStyle(
+                      color: Colors.white.withOpacity(0.5), fontSize: 14.0),
+                ),
+              ],
+            ),
+            const Spacer(),//maintain space b/w widgets in row/column
+            Icon(
+              Icons.more_horiz,
+              color: Colors.white.withOpacity(0.6),
+              size: 32.0,
+            ),
+
+            Icon(
+              Icons.favorite,
+              color: Colors.white.withOpacity(0.6),
+              size: 12.0,
+
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[900],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[800],
+        title: Text(
+          "Playlist",
+          style: TextStyle(
+              color: Colors.white70,
+              fontStyle: FontStyle.italic,
+              fontSize: 25.0),
+        ),
+      ),
+      body:
+      Column(children: [
+
+        Expanded(
+          child: ListView.builder(
+              itemCount: getList().length,
+              itemBuilder: (context, index) => customListView(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          MusicApp(msong: song[index]),
+                    ),);
+                },
+                title: song[index].title,
+                singer: song[index].singer,
+                image: song[index].image,
+                path: song[index].path,
+
+              )),
+        ),
+      ],),
+    );
+
+  }
+}
 
 
 
